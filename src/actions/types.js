e5e1f91dbe3ec42e6bd9f60b90acba9aa89b205d
Blake <bpro0821@gmail.com>
2020-04-17 11:43:37 -0500

    further scaffolding, breaking down the problem

---
+++ src/actions/types.js
@@ -0,0 +1,8 @@
+export const GET_CONFIG = 'GET_CONFIG';
+export const GET_INVOICES = 'GET_INVOICES';
+export const GET_VENDORS = 'GET_VENDORS';
+export const APPLY_CREDIT = 'APPLY_CREDIT';
+export const POST_PAYMENT = 'POST_PAYMENT';
+
+
+
